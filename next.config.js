d43ed4c43d8103ad7092303399cd2db7a6661ed5
laadga <galaadlureau66@gmail.com>
2025-08-19 17:58:53 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com', 'pub-940ccf6255b54fa799a9b01050e6c227.r2.dev', 'logo.clearbit.com'],
+  }
+}
+
+module.exports = nextConfig
